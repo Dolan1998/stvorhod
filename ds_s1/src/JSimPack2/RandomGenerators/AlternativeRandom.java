307a32c9400f54c0c9f246d157585960113eb26d
roman.kleman <roman.kleman@cfa36e54-058f-11de-a42d-873b86d837ff>
2009-03-02 07:52:35 +0000

    jfreeChart funguje aj s javadoc

--- ds_s1/src/JSimPack2/RandomGenerators/AlternativeRandom.java
+++ ds_s1/src/JSimPack2/RandomGenerators/AlternativeRandom.java
@@ -1,51 +1,50 @@
 package JSimPack2.RandomGenerators;
 
 public class AlternativeRandom extends AbstractRandom {
+
     private double[] probabilities;
-    
+
     public AlternativeRandom(double[] probabilities) {
         this.probabilities = new double[probabilities.length];
-	this.probabilities[0] = probabilities[0];
-	for (int i = 1; i < probabilities.length; i++) {
+        this.probabilities[0] = probabilities[0];
+        for (int i = 1; i < probabilities.length; i++) {
             this.probabilities[i] = this.probabilities[i - 1] + probabilities[i];
         }
     }
-    
+
     public AlternativeRandom(int[] probabilities) {
         this.probabilities = new double[probabilities.length];
-	this.probabilities[0] = probabilities[0];
-	for (int i = 1; i < probabilities.length; i++) {
+        this.probabilities[0] = probabilities[0];
+        for (int i = 1; i < probabilities.length; i++) {
             this.probabilities[i] = this.probabilities[i - 1] + probabilities[i];
         }
     }
-    
+
     public double nextDouble() {
-	double h = generator.nextDouble() * probabilities[probabilities.length - 1];
-	
-	if (h <= probabilities[0]) {
+        double h = generator.nextDouble() * probabilities[probabilities.length - 1];
+
+        if (h <= probabilities[0]) {
             return 0;
         }
-	for (int i = 1; i < probabilities.length - 1; i++) {
-	    if (h > probabilities[i - 1] && h <= probabilities[i]) {
+        for (int i = 1; i < probabilities.length - 1; i++) {
+            if (h > probabilities[i - 1] && h <= probabilities[i]) {
                 return i;
             }
-	}
-	return probabilities.length - 1;
+        }
+        return probabilities.length - 1;
     }
 
     public int nextInt() {
-	double h = generator.nextDouble() * probabilities[probabilities.length - 1];
-	
-	if (h <= probabilities[0]) {
+        double h = generator.nextDouble() * probabilities[probabilities.length - 1];
+
+        if (h <= probabilities[0]) {
             return 0;
         }
-	for (int i = 1; i < probabilities.length - 1; i++) {
-	    if (h > probabilities[i - 1] && h <= probabilities[i]) {
+        for (int i = 1; i < probabilities.length - 1; i++) {
+            if (h > probabilities[i - 1] && h <= probabilities[i]) {
                 return i;
             }
-	}
-	return probabilities.length - 1;
+        }
+        return probabilities.length - 1;
     }
-
-
 }
--- ds_s1/src/JSimPack2/RandomGenerators/TriangularRandom.java
+++ ds_s1/src/JSimPack2/RandomGenerators/TriangularRandom.java
@@ -1,31 +1,32 @@
 package JSimPack2.RandomGenerators;
 
 public class TriangularRandom extends AbstractRandom {
-    private double a, b, m;
-    
+
+    private double a,  b,  m;
+
     public TriangularRandom(double a, double b, double m) {
-	this.a = a;
-	this.b = b;
-	this.m = m;
+        this.a = a;
+        this.b = b;
+        this.m = m;
     }
 
     public double nextDouble() {
         double h = generator.nextDouble() * (b - a) + a;
-        
-	if (h < m) {
-            return Math.sqrt((h-a) * (m-a)) + a;
+
+        if (h < m) {
+            return Math.sqrt((h - a) * (m - a)) + a;
         } else {
-            return b - Math.sqrt((b-h) * (b-m));
+            return b - Math.sqrt((b - h) * (b - m));
         }
     }
 
     public int nextInt() {
         double h = generator.nextDouble() * (b - a) + a;
-        
-	if (h < m) {
-            return Math.round((float)(Math.sqrt((h-a) * (m-a)) + a));
+
+        if (h < m) {
+            return Math.round((float) (Math.sqrt((h - a) * (m - a)) + a));
         } else {
-            return Math.round((float)(b - Math.sqrt((b-h) * (b-m))));
+            return Math.round((float) (b - Math.sqrt((b - h) * (b - m))));
         }
     }
 }
